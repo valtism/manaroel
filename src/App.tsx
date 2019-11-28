0993d0903790bcad8b73559904b54c7ac197ccaf
Dan Woo <daniel4wood@icloud.com>
2019-11-28 11:00:18 +0100

    Setup theme-ui and normalized css
    The first steps for scaffolding. Have decided to try using Theme UI
    with a tailwind preset. Have also added tailwind's preflight, which is
    an opinionated normalize.css
    
    Also had to add an @theme-ui/presets typing file. Have opened an issue
    for adding this typing into DefinitelyTyped, but I don't know how to
    contribute myself.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { ThemeProvider, jsx } from "theme-ui";
+import theme from "./theme";
+import React from "react";
+import Layout from "./Layout";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <h1>Appy</h1>
+      </Layout>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/Layout.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React from "react";
+
+const Layout: React.FC = props => (
+  <div>
+    <div>aa</div>
+    {props.children}
+  </div>
+);
+
+export default Layout;
